f38a58e682ce4059b85b7fb1ac173a0be53cf5e9
WalkerJDG <juandanielgomezcorrea77@outlook.com>
2025-09-18 15:29:59 -0500

    Initial commit for the MiniJuego project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Producto
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("¡Bienvenido al programa en C#!");
+        Console.Write("Por favor, escribe tu nombre: ");
+        string nombre = Console.ReadLine();
+        Console.Write("Ahora escribe tu edad: ");
+        string edad = Console.ReadLine();
+        string mensaje = $"Bienvenido {nombre}, tienes {edad} años.";
+        Console.WriteLine(mensaje);
+    }
+}
